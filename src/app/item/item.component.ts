f711ce8fec075c490cf1e2371824430d304ecc4c
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-11-10 15:24:17 +0300

    Work with request parameters

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,21 +1,28 @@
 import { Component} from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute, ParamMap} from '@angular/router';
   
 @Component({
     selector: 'item-info',
-    template: `<h3>Модель {{id}}</h3>`
+    template: `<h3>Модель {{id}}</h3>
+    <div>Товар: {{product}}</div>
+    <div>Цена: {{price}}</div>`
 })
 export class ItemComponent { 
      
-    id: number | undefined;
+    id: any;
+    product: any;
+    price: any;
      
     constructor(private route: ActivatedRoute){}
 
     ngOnInit() {
-        this.route.paramMap.pipe(
-            switchMap(params => params.getAll('id'))
-        )
-        .subscribe(data=> this.id = +data);
+        this.route.paramMap
+        .subscribe((data: ParamMap) => this.id = +data.getAll('id'));
+
+        this.route.queryParamMap
+        .subscribe((data: ParamMap) => {
+            this.product = data.getAll('product');
+            this.price = data.getAll('price');
+        });
       }
 }
